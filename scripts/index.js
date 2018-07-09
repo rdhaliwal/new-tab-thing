58ac679c699323063fe2d18dc2adbd8d39cc191a
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2018-07-09 21:13:24 +1000

    Finish up reading config from JSON file
     - Doesn't render the HTML from it yet

--- scripts/index.js
+++ scripts/index.js
@@ -22,47 +22,36 @@
     let adjective = ADJECTIVES[Math.floor(Math.random() * ADJECTIVES.length)];
     let noun = NOUNS[Math.floor(Math.random() * NOUNS.length)];
     let compliment = document.querySelector("#Compliment");
-    compliment.innerHTML = `Have a wonderful day ${NAME}, you ${adjective} ${noun}!`;
+    return `Have a wonderful day ${NAME}, you ${adjective} ${noun}!`;
   }
 
-  const generateColors = () => {
-    let main = document.querySelector("#Main");
-    const NUMBER_OF_COLOR_SCHEMES = 2;
-    let chosenScheme =  Math.floor(Math.random() * NUMBER_OF_COLOR_SCHEMES) + 1;
-    main.classList.add(`Main--scheme${chosenScheme}`);
-  }
-
-  document.addEventListener('DOMContentLoaded', function() {
-    generateColors();
-    generateCompliment();
-  }, false);
-})();
-
-
-(() => {
   const readConfig = () => {
     return new Promise((resolve, reject) => {
       chrome.runtime.getPackageDirectoryEntry((root) => {
         const errorHandler = (e) => {
           console.log(e);
           reject(e);
-        }; 
+        };
 
         root.getFile("scripts/config.json", {}, (fileEntry) => {
           fileEntry.file((file) => {
             var reader = new FileReader();
             reader.onloadend = function(e) {
-              resolve(JSON.parse(this.result)); 
+              resolve(JSON.parse(this.result));
             };
             reader.readAsText(file);
           }, errorHandler);
         }, errorHandler);
-      }); 
-    })  
+      });
+    })
   }
 
   document.addEventListener('DOMContentLoaded', async () => {
-    window.config = await readConfig();
+    const config = await readConfig();
+    const compliment = generateCompliment();
+
+    console.log(config);
+    console.log(compliment);
   }, false);
 })();
 
